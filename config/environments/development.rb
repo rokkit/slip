07e675bd3e50a9bec7e7696669c0e7884f99887a
Maxim Indykov <indmaksim@gmail.com>
2013-11-15 18:06:34 +0400

    fix development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,8 @@ Slip::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
